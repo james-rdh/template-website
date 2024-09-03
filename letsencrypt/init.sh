89eaafbfc7988f72cc2166f8d86e860298c6ccf0
James Hamilton <jamesrdhamilton@gmail.com>
2024-09-03 04:20:00 +0100

    Configuration change

--- letsencrypt/init.sh
+++ letsencrypt/init.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
-  exit 1
-fi
-
-domains=(example.org www.example.org)
+domains=(james-hamilton.uk jameshamiltondata.com jameshamiltonenergy.com)
+cert_name=jameshamilton
 rsa_key_size=4096
-data_path="./data/certbot"
+path="/etc/letsencrypt/live/$cert_name"
 email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
-fi
+# if [ -d "$path" ]; then
+#   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+#   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+#     exit
+#   fi
+# fi
 
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-  echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-  echo
-fi
+# if [ ! -e "$path/options-ssl-nginx.conf" ] || [ ! -e "$path/ssl-dhparams.pem" ]; then
+#   echo "### Downloading recommended TLS parameters ..."
+#   mkdir -p "$path"
+#   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/options-ssl-nginx.conf"
+#   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$path/ssl-dhparams.pem"
+#   echo
+# fi
 
 echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
@@ -42,9 +36,9 @@ echo
 
 echo "### Deleting dummy certificate for $domains ..."
 docker-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  rm -Rf /etc/letsencrypt/live/$cert_name && \
+  rm -Rf /etc/letsencrypt/archive/$cert_name && \
+  rm -Rf /etc/letsencrypt/renewal/$cert_name.conf" certbot
 echo
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
@@ -64,10 +58,11 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
+  certbot certonly --webroot -w /var/www/html \
     $staging_arg \
     $email_arg \
     $domain_args \
+    --certname $cert_name
     --rsa-key-size $rsa_key_size \
     --agree-tos \
     --force-renewal" certbot
@@ -75,3 +70,10 @@ echo
 
 echo "### Reloading nginx ..."
 docker-compose exec nginx nginx -s reload
+
+# Renewal loop
+trap exit TERM; while :; do
+    certbot renew; 
+    sleep 12h & 
+    wait $${!}
+done;
---
+++ nginx/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while :; do
+  sleep 6h &
+  wait $${!};
+  nginx -s reload;
+done &
+nginx -g "daemon off;"
