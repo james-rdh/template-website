39928b7f7b2c06a89a9f385d9c63494b5fd4b83e
James Hamilton <jamesrdhamilton@gmail.com>
2024-09-03 04:38:57 +0100

    Configuration change

--- letsencrypt/init.sh
+++ letsencrypt/init.sh
@@ -5,7 +5,6 @@ cert_name=jameshamilton
 rsa_key_size=4096
 path="/etc/letsencrypt/live/$cert_name"
 email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 # if [ -d "$path" ]; then
 #   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -54,17 +53,14 @@ case "$email" in
   *) email_arg="--email $email" ;;
 esac
 
-# Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
-
 docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/html \
-    $staging_arg \
     $email_arg \
     $domain_args \
     --certname $cert_name
     --rsa-key-size $rsa_key_size \
     --agree-tos \
+    --no-eff-email
     --force-renewal" certbot
 echo
 
