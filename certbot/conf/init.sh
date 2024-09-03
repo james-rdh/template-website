17a916e3d9df9e149232763d6be9c685be10ad4b
James Hamilton <jamesrdhamilton@gmail.com>
2024-09-03 07:48:23 +0100

    Configuration change

--- certbot/conf/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Renewal loop
-trap exit TERM;
-while :; do
-  certbot renew; 
-  sleep 12h & 
-  wait $${!}
-done
--- nginx/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while :; do
-  sleep 6h &
-  wait $${!};
-  nginx -s reload;
-done &
-nginx -g "daemon off;"
