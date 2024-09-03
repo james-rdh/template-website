6be588d06aab36ad3391fb49cdc74685aa68762f
James Hamilton <jamesrdhamilton@gmail.com>
2024-09-03 07:54:01 +0100

    Configuration change

--- init.sh
+++ init.sh
@@ -3,15 +3,16 @@
 domains=(james-hamilton.uk jameshamiltondata.com jameshamiltonenergy.com)
 cert_name=jameshamilton
 rsa_key_size=4096
-path="/etc/letsencrypt/live/$cert_name"
+certbot_path="/etc/letsencrypt/live/$cert_name"
+host_path="./certbot/conf/live/$cert_name"
 email="" # Adding a valid address is strongly recommended
 
-# if [ -d "$path" ]; then
-#   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-#   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-#     exit
-#   fi
-# fi
+if [ -d "$host_path" ]; then
+  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+    exit
+  fi
+fi
 
 # if [ ! -e "$path/options-ssl-nginx.conf" ] || [ ! -e "$path/ssl-dhparams.pem" ]; then
 #   echo "### Downloading recommended TLS parameters ..."
@@ -22,8 +23,8 @@ email="" # Adding a valid address is strongly recommended
 # fi
 
 echo "### Creating dummy certificate for $domains ..."
-sudo mkdir -p "./certbot/conf/live/$cert_name/"
-docker compose run --rm --entrypoint "openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 -keyout '$path/privkey.pem' -out '$path/fullchain.pem' -subj '/CN=localhost'" certbot
+sudo mkdir -p "$host_path"
+docker compose run --rm --entrypoint "openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 -keyout '$certbot_path/privkey.pem' -out '$certbot_path/fullchain.pem' -subj '/CN=localhost'" certbot
 echo
 
 echo "### Starting nginx ..."
@@ -31,7 +32,7 @@ docker compose up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker compose run --rm --entrypoint "rm -Rf $path && rm -Rf /etc/letsencrypt/archive/$cert_name && rm -Rf /etc/letsencrypt/renewal/$cert_name.conf" certbot
+docker compose run --rm --entrypoint "rm -Rf $certbot_path && rm -Rf /etc/letsencrypt/archive/$cert_name && rm -Rf /etc/letsencrypt/renewal/$cert_name.conf" certbot
 echo
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
